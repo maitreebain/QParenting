be7d452bbd186d32dd07bad3863f26aa7b05b2b0
maitreebain <maitreebain@gmail.com>
2020-09-02 12:55:15 -0400

    setting up project

---
+++ QParenting/QParenting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QParenting
+//
+//  Created by Maitree Bain on 9/2/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
