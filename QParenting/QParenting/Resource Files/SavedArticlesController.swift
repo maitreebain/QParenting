02061b7d06a8af87a1c167469b301dbb104c5887
maitreebain <maitreebain@gmail.com>
2020-10-23 17:08:17 -0400

    loaded persisted articles

--- QParenting/QParenting/Resource Files/SavedArticlesController.swift
+++ QParenting/QParenting/Resource Files/SavedArticlesController.swift
@@ -7,29 +7,85 @@
 //
 
 import UIKit
+import DataPersistence
 
 class SavedArticlesController: UIViewController {
     
     @IBOutlet var savedArticlesCollection: UICollectionView!
     
+    private var dataPer = DataPersistence<SiteInfo>(filename: "savedArticles")
+    var imageNames = ["prideB", "prideC", "prideD", "prideE", "prideF"]
+    var imagesArr = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        savedArticlesCollection.delegate = self
-//        savedArticlesCollection.dataSource = self
+        randomImages()
+        savedArticlesCollection.delegate = self
+        savedArticlesCollection.dataSource = self
+        savedArticlesCollection.register(UINib(nibName: "ResourceCell", bundle: nil), forCellWithReuseIdentifier: ResourceCell.reuseIdentifier)
+    }
+    
+    func getData() -> [SiteInfo] {
+        do {
+            return try dataPer.loadItems()
+        } catch {
+            print("couldnt load data \(error)")
+        }
+        return []
+    }
+    
+    func randomImages() {
+        for name in imageNames {
+            let img = UIImage(named: name)
+            if let image = img {
+                imagesArr.append(image)
+            }
+        }
+    }
+    
+}
+
+extension SavedArticlesController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        cell.colorShadow(for: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width * 0.9
+        let itemHeight: CGFloat = maxSize.height * 0.2
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return getData().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResourceCell.reuseIdentifier, for: indexPath) as? ResourceCell else {
+            fatalError("could not dequeue a LabelCell")
+        }
+        cell.resourceImage.image = self.imagesArr[indexPath.row % self.imagesArr.count]
+        let resource = getData()[indexPath.row]
+        cell.configureCell(resource)
+        cell.delegate = self
+        
+        return cell
     }
     
     
 }
 
-//extension SavedArticlesController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//    
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//    
-//    }
-//    
-//    
-//}
+extension SavedArticlesController: SavedArticleDelegate {
+    
+    func didSaveArticle(_ cell: UICollectionViewCell, article: SiteInfo) {
+    }
+    
+    
+}
